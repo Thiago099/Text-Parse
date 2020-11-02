9aa4d43f1ecb261c0a449d3381dfa9c94434f284
Thiago Kaique Ribeiro Fonseca <66787043+Thiago099@users.noreply.github.com>
2020-11-02 10:09:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let name_sex=["male","female"]
+let color_sex=["#8AA6E9","#E196B3"]
+
+let dic=dictionary(
+  [
+    "joao",
+    "maria",
+    "paulo",
+    "bruna",
+  ],
+  [0,1,0,1]
+)
+
+let sex=(name)=>{
+  let m=dictate(dic,name);
+  write_line(name,m.found?color_sex[m.value]:"magenta")
+}
+sex("joao");
+sex("bruna");
+sex("pedro");
+
+
+text="Joao tem 2 amigos, Maria e Paulo.";
+write_color(text);
+
+parse(text).forEach((item) => {
+  write(item.value,apearence_color[item.type]);
+
+});
