2781c49d0681b11232bf04b97821efbb8e5e0424
Thiago Kaique Ribeiro Fonseca <66787043+Thiago099@users.noreply.github.com>
2020-11-07 21:08:10 -0300

    Update dictionary.js

--- bin/dictionary.js
+++ bin/dictionary.js
@@ -1,34 +1,34 @@
-const find=(array,value)=>{
+const find=(dictionary,value)=>{
   let l = 0;
-  let r = array.length-1;
+  let r = dictionary.length-1;
   let c = 0;
   let com = 0;
   while (l <= r)
   {
     c = Math.floor((l + r) / 2);
-    com = array[c].index-value;
+    com = dictionary[c].index-value;
     if (com > 0) r = c - 1;
     else if (com < 0) l = c + 1;
-    else return {value:array[c].value,found:true};
+    else return {value:dictionary[c].value,found:true};
   }
   return {id:c,found:false};
 }
 
-const mind=(array,value)=>{
+const mind=(dictionary,value)=>{
   let l = 0;
-  let r = array.length-1;
+  let r = dictionary.length-1;
   let c = 0;
   let com = 0;
   while (l <= r)
   {
     c = Math.floor((l + r) / 2);
-    com = array[c].index-value;
+    com = dictionary[c].index-value;
     if (com > 0) r = c - 1;
     else if (com < 0) l = c + 1;
     else return c;
   }
-  if (c >= array.length)
-      return array.length;
+  if (c >= dictionary.length)
+      return dictionary.length;
   if (com < 0) c++;
       return c;
 }
@@ -37,10 +37,9 @@ const match=(array,index,value)=>
        if(index>=array.length) array.push(value);
   else                         array.splice(index,0, value);
 }
-
-const append=(array,value)=>
+const append=(dictionary,value)=>
 {
-  match(array,mind(array,value),value);
+  match(dictionary,mind(dictionary,value.index),value);
 }
 const dictionary=(array,value)=>
 {
@@ -48,7 +47,7 @@ const dictionary=(array,value)=>
   for (let i = 0; i < array.length; i++) {
     let item = array[i];
     let h=hash(item);
-    match(ret,mind(ret,h),{index:h,value:value[i]})
+    append(ret,{index:h,value:value[i]})
   };
   return ret;
 }
